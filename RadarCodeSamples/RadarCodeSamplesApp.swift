1886aebb6c0451c8b389e0608c9c38109a150039
JoRoRadar <joe.ross@radar.io>
2022-03-22 13:51:26 -0700

    Initial Commit

---
+++ RadarCodeSamples/RadarCodeSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadarCodeSamplesApp.swift
+//  RadarCodeSamples
+//
+//  Created by Joe Ross on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RadarCodeSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
